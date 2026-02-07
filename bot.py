c2411f6a7dc1c3268823a73ff28008224bfd1271
drexocoder-source <drexocoder@gmail.com>
2026-02-07 20:18:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,6 @@ from database.connection import db
 import asyncio
 from database.migrate import migrate
 
-async def initialize_database():
-    """Call this inside your main bot startup function."""
-    await db.connect()
-    await migrate()
-    print("✅ Database connected & tables ready")
 
 async def start_nexora():
     # 1. Initialize Database inside the running loop
