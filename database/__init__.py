6a7a3e26ff632c628813f1155abe73925a8775d0
drexocoder-source <drexocoder@gmail.com>
2026-02-07 20:23:07 +0530

    Delete database/__init__.py

--- database/__init__.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from database.connection import db
-from database.migrate import migrate
-
-async def initialize_database():
-    """Call this inside your main bot startup function."""
-    await db.connect()
-    await migrate()
-    print("✅ Database connected & tables ready")
